fe941ed8ff9bd0b9724e421fc73777f661ebebed
afermanx <alexjferman@gmail.com>
2022-09-26 09:39:54 -0400

    spread use

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace App\Services;
+
 use App\Models\User;
 use App\Traits\ApiException;
 use Illuminate\Support\Facades\Hash;
@@ -30,15 +31,9 @@ class UserService
      */
     public function create(array $data): User
     {
-
         return User::create([
-            'institution_id' => data_get($data, 'institution_id'),
-            'name' => data_get($data, 'name'),
-            'cpf' => data_get($data, 'cpf'),
-            'email' => data_get($data, 'email'),
+            ...$data,
             'password' => Hash::make(data_get($data, 'password')),
-            'is_admin' => data_get($data, 'is_admin'),
         ]);
     }
-
 }
