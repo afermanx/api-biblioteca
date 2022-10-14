38bf0cb53d1643fdfc8f9e53fc2bc62b5a5482f5
afermanx <alexjferman@gmail.com>
2022-10-13 23:05:14 -0400

    fix books

---
+++ database/migrations/2022_10_11_024502_create_books_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Institution;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Institution::class);
+            $table->string('name');
+            $table->string('description');
+            $table->string('classification');
+            $table->string('author');
+            $table->string('publisher');
+            $table->enum('status', ['inactive', 'active','absent'])->default('active');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+};
