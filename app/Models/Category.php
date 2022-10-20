9e5a523b8ab7703973834a4814ba2d7f8e5a9388
afermanx <alexjferman@gmail.com>
2022-10-19 22:20:39 -0400

    Model and migrayion category book an relationship

---
+++ app/Models/Category.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Category extends Model
+{
+    use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $filable = [
+        'name'
+    ];
+
+    /**
+     * The books that belong to the Category
+     *
+     * @return BelongsToMany
+     */
+    public function books(): BelongsToMany
+    {
+        return $this->belongsToMany(Book::class);
+    }
+}
---
+++ database/migrations/2022_10_20_003700_create_book_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Book;
+use App\Models\Category;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('book_categories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(Book::class);
+            $table->foreignIdFor(Category::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('book_categories');
+    }
+};
